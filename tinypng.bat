47fe163a99712f6353e33c430f28f17ed630d391
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2017-05-08 12:16:03 -0300

    initial commit

---
+++ tinypng.bat
@@ -0,0 +1,25 @@
+@echo off
+
+setlocal
+
+:: you can get APIKEY from https://tinypng.com/developers
+:: set APIKEY=
+ 
+:: if set prefix or (and) suffix, not overwrite original file.
+set PREFIX=
+set SUFFIX=
+
+for /f "tokens=2 delims=)" %%i in ('dir *.jpg *.png ^| grep -r+ "bytes$"') do @set before=%%i
+ 
+for %%p in ( *.png,*.jpg ) do (
+  echo.
+  echo Compressing %%p...	
+  for /f "tokens=1,2 usebackq" %%j in ( `curl -k -i -# --user api:%APIKEY% --data-binary @"%%p" https://api.tinypng.com/shrink` ) do (
+    if "%%j"=="Location:" curl -k %%k -# > %PREFIX%%%p%SUFFIX%
+  )
+)
+
+for /f "tokens=2 delims=)" %%i in ('dir *.jpg *.png ^| grep -r+ "bytes$"') do @set after=%%i
+
+echo Before: %before%
+echo After: %after%
