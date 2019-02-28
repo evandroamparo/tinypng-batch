259ae3f3c6ad71517bd0175651f70a5d830472f7
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2019-02-28 11:24:12 -0300

    change environment variable for api key

--- tinypng.bat
+++ tinypng.bat
@@ -3,7 +3,7 @@
 setlocal
 
 :: you can get APIKEY from https://tinypng.com/developers
-:: set APIKEY=
+:: set TINYPNG_APIKEY=
  
 :: if set prefix or (and) suffix, not overwrite original file.
 set PREFIX=
@@ -14,8 +14,8 @@ for /f "tokens=2 delims=)" %%i in ('dir *.jpg *.png ^| grep -r+ "bytes$"') do @s
 for %%p in ( *.png,*.jpg ) do (
   echo.
   echo Compressing %%p...	
-  for /f "tokens=1,2 usebackq" %%j in ( `curl -k -i -# --user api:%APIKEY% --data-binary @"%%p" https://api.tinypng.com/shrink` ) do (
-    if "%%j"=="Location:" curl --user api:%APIKEY% --header "Content-Type: application/json" --data "{\"preserve\": [\"copyright\", \"location\", \"creation\"]}" -k %%k -# > %PREFIX%%%p%SUFFIX%
+  for /f "tokens=1,2 usebackq" %%j in ( `curl -k -i -# --user api:%TINYPNG_APIKEY% --data-binary @"%%p" https://api.tinypng.com/shrink` ) do (
+    if "%%j"=="Location:" curl --user api:%TINYPNG_APIKEY% --header "Content-Type: application/json" --data "{\"preserve\": [\"copyright\", \"location\", \"creation\"]}" -k %%k -# > %PREFIX%%%p%SUFFIX%
   )
 )
 
